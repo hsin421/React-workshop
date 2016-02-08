a9947d83596be09853319708ca9babb51896e993
Hsin <shinsonh@gmail.com>
2016-02-08 14:52:22 -0500

    adding subjects

--- subjects/00-hello-world/exercise.js
+++ subjects/00-hello-world/exercise.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  render() {
-    return <h1>Hello World!</h1>
-  }
+//   render() {
+//     return React.createElement('h1', {className: 'my-heading'}, 'hello world!!');
+//   }
 
+// }
+
+const App = function() {
+	return React.createElement('h1', {className: 'my-heading'}, 'hello world');
 }
 
 render(<App />, document.getElementById('app'))
